8f52a786623955472bfaa845e73bfd6a582bbcbe
Andrew Li <andrewli@u.northwestern.edu>
2025-05-29 02:36:02 -0500

    reformat

--- celebi/blueprints/wildcat/api.py
+++ celebi/blueprints/wildcat/api.py
@@ -2,16 +2,12 @@
 API routes for parser blueprint
 """
 
-import requests
 
 from http import HTTPStatus
-from flask import Blueprint, request, Response
+from flask import Blueprint, Response
 from flask_cors import cross_origin
 
-from celebi.core.config import AppConfig
 from celebi.core.util import make_json_response
-from celebi.core.util import get_body_field
-from celebi.core.exceptions import RequestValueError
 
 
 bp = Blueprint("wildcat", __name__)
@@ -27,4 +23,6 @@ def subscribe_new_email() -> Response:
     can be found in the commit history of this repository.
     """
 
-    return make_json_response("WildHacks 2025 is over. Please subscribe to the mailing list for WildHacks 2026.", HTTPStatus.BAD_REQUEST)
+    return make_json_response(
+        "WildHacks 2025 is over. Please subscribe to the mailing list for WildHacks 2026.", HTTPStatus.BAD_REQUEST
+    )
