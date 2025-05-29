977944c5fd5c51e786297bd4465ef1257174f021
Andrew Li <andrewli@u.northwestern.edu>
2025-05-29 02:35:47 -0500

    remove unreachable code

--- celebi/blueprints/wildcat/api.py
+++ celebi/blueprints/wildcat/api.py
@@ -22,56 +22,9 @@ bp = Blueprint("wildcat", __name__)
 def subscribe_new_email() -> Response:
     """
     Subscribes a new user to the Wildhacks email list
+
+    Deprecated! A new route is needed for WildHacks 2026. The code for the existing route
+    can be found in the commit history of this repository.
     """
 
     return make_json_response("WildHacks 2025 is over. Please subscribe to the mailing list for WildHacks 2026.", HTTPStatus.BAD_REQUEST)
-
-    request_body = request.get_json()
-    if not request_body:
-        msg = "Request did not contain a JSON body"
-        return make_json_response(msg, HTTPStatus.BAD_REQUEST)
-
-    try:
-        first_name = get_body_field(request_body, "first_name")
-        last_name = get_body_field(request_body, "last_name")
-        email_address = get_body_field(request_body, "email_address")
-    except RequestValueError as e:
-        return make_json_response(str(e), HTTPStatus.BAD_REQUEST)
-
-    app_config = AppConfig()
-    api_key = app_config.get_environment_variable("MAILCHIMP_API_KEY")
-    list_id = app_config.get_environment_variable("MAILCHIMP_EMAIL_LIST")
-    mailchimp = f"https://us11.api.mailchimp.com/3.0/lists/{list_id}/members"
-
-    post_request_body = {
-        "email_address": email_address,
-        "status": "subscribed",
-        "merge_fields": {
-            "FNAME": first_name,
-            "LNAME": last_name,
-        },
-    }
-
-    request_timeout_seconds = 5
-    res = requests.post(
-        mailchimp,
-        auth=("hi", api_key),
-        json=post_request_body,
-        timeout=request_timeout_seconds,
-    )
-    response_title = res.json().get("title", "NO TITLE")
-
-    if res.status_code == 200:
-        msg = f"Successfully subscribed user {email_address}"
-        return make_json_response(msg, HTTPStatus.OK)
-    elif response_title == "Member Exists":
-        msg = f"User {email_address} already exists"
-        return make_json_response(msg, HTTPStatus.CONFLICT)
-    elif res.status_code == 400:
-        return make_json_response(
-            f"Failed to subscribe user {email_address}",
-            HTTPStatus.INTERNAL_SERVER_ERROR,
-        )
-    else:
-        msg = f"Encountered status code {res.status_code}"
-        return make_json_response(msg, res.status_code)
